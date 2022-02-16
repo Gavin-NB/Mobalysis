7c3fc7f143330297d6a0b10d3fada5423b9fac77
jen <jenjepkirui@gmail.com>
2022-02-16 17:06:23 +0300

    variable update on bashrc file

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 cd /home/mob_app_usr
 sudo git clone https://github.com/Gavin-NB/Mobalysis.git
 sudo echo "export DBNAME=mobalytics" >> .bashrc
-sudo echo "export DBUSER=mob_db_user" >>.bashrc
-sudo echo "export DBPASS=mob_db_pass" >>.bashrc
-sudo echo "export DBHOST=localhost" >>.bashrc
-sudo echo "export DBPORT=5432"  >>.bashrc
+sudo echo "export DBUSER=mob_db_user" >> .bashrc
+sudo echo "export DBPASS=mob_db_pass" >> .bashrc
+sudo echo "export DBHOST=localhost" >> .bashrc
+sudo echo "export DBPORT=5432"  >> .bashrc
